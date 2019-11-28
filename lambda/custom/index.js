8b7c21b89c823cae4b48563c1727668531ce0af7
Jack <jackcorbett1998@hotmail.co.uk>
2019-11-28 11:18:12 +0000

    The Skilll fetches the users address

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,82 +1,150 @@
 const Alexa = require('ask-sdk-core');
 
+const skillName = 'Local Facts';
+
+const messages = {
+  WELCOME: 'Welcome to local facts!',
+  HELP: 'You can say tell me a local fact, or, you can say exit... What can I help you with?',
+  HELP_REPROMPT: 'What can I help you with?',
+  FALLBACK: 'The Local Facts skill can\'t help you with that. It can help you discover facts about your local area if you say tell me a local fact.',
+  FALLBACK_REPROMPT: 'What can I help you with?',
+  NOTIFY_MISSING_PERMISSIONS: 'Please enable address permissions in the Amazon Alexa app to find out facts about your local area. Then start local facts again.',
+  LOCATION_FAILURE: 'There was a problem getting your address, please try again later.',
+  ERROR: 'Sorry, an error occurred.',
+  STOP: 'Goodbye!',
+}
+
+const facts = [
+  'The New Forest is one of Britains newest and smallest national parks with an area of 218 square miles.',
+  'Hampshire is the birthplace of Jane Austen and Charles Dickens.',
+  'Southampton is the home of the Spitfire Aircraft which was designed by R.J. Mitchell.',
+  'Southampton was the first town in Britain to sample fish fingers in 1955.',
+  'The first Sherlock Holmes story was written in Southsea by Sir Arthur Conan-Doyle.'
+];
+
+const PERMISSIONS = ['read::alexa:device:all:address'];
+
 const LaunchRequestHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === `LaunchRequest`;
   },
   handle(handlerInput) {
     return handlerInput.responseBuilder
-      .speak(welcomeMessage)
-      .reprompt(helpMessage)
+      .speak(messages.WELCOME)
+      .reprompt(messages.HELP)
       .getResponse();
   },
 };
 
 const GetNewFactHandler = {
   canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
+    const { request } = handlerInput.requestEnvelope
     return request.type === 'IntentRequest' && request.intent.name === 'GetNewFactIntent';
   },
-  handle(handlerInput) {
-    console.log("Processing fact request");
-    // TODO: Fetch a fact based on the users location
-    // Get a random fact
-    const randomFact = facts[Math.floor(Math.random()*facts.length)];
+  async handle(handlerInput) {
+    const { requestEnvelope, serviceClientFactory, responseBuilder } = handlerInput;
+    
+    const consentToken = requestEnvelope.context.System.user.permissions
+      && requestEnvelope.context.System.user.permissions.consentToken;
+    
+    if (!consentToken) {
+      return responseBuilder
+        .speak(messages.NOTIFY_MISSING_PERMISSIONS)
+        .withAskForPermissionsConsentCard(PERMISSIONS)
+        .getResponse();
+    } 
+    try {
+      const { deviceId } = requestEnvelope.context.System.device;
+      const deviceAddressServiceClient = serviceClientFactory.getDeviceAddressServiceClient();
+      const address = await deviceAddressServiceClient.getFullAddress(deviceId);
 
-    return handlerInput.responseBuilder
-      .speak(randomFact)
-      .reprompt(helpReprompt)
-      .withSimpleCard(skillName, randomFact)
-      .getResponse();
+      console.log('Address successfully retrieved, now responding to user.');
+
+      // TODO: Fetch a fact based on the users location
+      // Currently get a random fact
+      const randomFact = facts[Math.floor(Math.random()*facts.length)];
+
+      return responseBuilder
+        .speak(randomFact + " County: " + address.stateOrRegion)
+        .reprompt(messages.HELP_REPROMPT)
+        .withSimpleCard(skillName, randomFact)
+        .getResponse();
+    } catch (error) {
+      if (error.name !== 'ServiceError') {
+        return responseBuilder 
+          .speak(messages.ERROR)
+          .reprompt(messages.ERROR)
+          .getResponse();
+      }
+      throw error;
+    }
   },
 };
 
 const HelpHandler = {
   canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
+    const { request } = handlerInput.requestEnvelope
     return request.type === 'IntentRequest' && request.intent.name === 'AMAZON.HelpIntent';
   },
   handle(handlerInput) {
     return handlerInput.responseBuilder
-      .speak(helpMessage)
-      .reprompt(helpReprompt)
+      .speak(messages.HELP)
+      .reprompt(messages.HELP_REPROMPT)
       .getResponse();
   },
 };
 
 const FallbackHandler = {
   canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
+    const { request } = handlerInput.requestEnvelope
     return request.type === 'IntentRequest' && request.intent.name === 'AMAZON.FallbackIntent';
   },
   handle(handlerInput) {
     return handlerInput.responseBuilder
-      .speak(fallbackMessage)
-      .reprompt(fallbackReprompt)
+      .speak(messages.FALLBACK)
+      .reprompt(messages.FALLBACK_REPROMPT)
       .getResponse();
   },
 };
 
 const ExitHandler = {
   canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
+    const { request } = handlerInput.requestEnvelope
     return request.type === 'IntentRequest' && (request.intent.name === 'AMAZON.CancelIntent' || request.intent.name === 'AMAZON.StopIntent');
   },
   handle(handlerInput) {
     return handlerInput.responseBuilder
-      .speak(stopMessage)
+      .speak(messages.STOP)
       .getResponse();
   },
 };
 
 const SessionEndedRequestHandler = {
   canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
+    const { request } = handlerInput.requestEnvelope
     return request.type === 'SessionEndedRequest';
   },
   handle(handlerInput) {
     console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
     return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+const GetAddressError = {
+  canHandle(handlerInput, error) {
+    return error.name === 'ServiceError';
+  },
+  handle(handlerInput, error) {
+    if (error.statusCode === 403) {
+      return handlerInput.responseBuilder
+        .speak(messages.NOTIFY_MISSING_PERMISSIONS)
+        .withAskForPermissionsConsentCard(PERMISSIONS)
+        .getResponse();
+    }
+    return handlerInput.responseBuilder
+      .speak(messages.LOCATION_FAILURE)
+      .reprompt(messages.LOCATION_FAILURE)
+      .getResponse();
   },
 };
 
@@ -88,8 +156,8 @@ const ErrorHandler = {
     console.log(`Error handled: ${error.message}`);
     console.log(`Error stack: ${error.stack}`);
     return handlerInput.responseBuilder
-      .speak(errorMessage)
-      .reprompt(errorMessage)
+      .speak(messages.ERROR)
+      .reprompt(messages.ERROR)
       .getResponse();
   },
 };
@@ -105,23 +173,6 @@ exports.handler = skillBuilder
     FallbackHandler,
     SessionEndedRequestHandler,
   )
-  .addErrorHandlers(ErrorHandler)
+  .addErrorHandlers(ErrorHandler, GetAddressError)
+  .withApiClient(new Alexa.DefaultApiClient())
   .lambda();
-
-const skillName = 'Local Facts';
-
-const welcomeMessage = 'Welcome to local facts! You can say "tell me a fact" to find out about your local area.';
-const helpMessage = 'You can say tell me a local fact, or, you can say exit... What can I help you with?';
-const helpReprompt = 'What can I help you with?';
-const fallbackMessage = 'The Local Facts skill can\'t help you with that. It can help you discover facts about your local area if you say tell me a local fact. What can I help you with?';
-const fallbackReprompt = 'What can I help you with?';
-const errorMessage = 'Sorry, an error occurred.';
-const stopMessage = 'Goodbye!';
-
-const facts = [
-  'The New Forest is one of Britains newest and smallest national parks with an area of 218 square miles.',
-  'Hampshire is the birthplace of Jane Austen and Charles Dickens.',
-  'Southampton is the home of the Spitfire Aircraft which was designed by R.J. Mitchell.',
-  'Southampton was the first town in Britain to sample fish fingers in 1955.',
-  'The first Sherlock Holmes story was written in Southsea by Sir Arthur Conan-Doyle.'
-];
